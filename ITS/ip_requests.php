458568057b8eb6f881fcd9da6ae240a9ad1097b5
romanzc2011 <romanzc2011@gmail.com>
2024-04-26 18:44:34 -0500

    Adding database functionality to ip_requests

---
+++ ITS/ip_requests.php
@@ -0,0 +1,88 @@
+<?php require_once('../controller/raspberrypi_mysql_connection.php'); ?>
+
+<?php
+
+global $MYSQL_CONN;
+$student_id = "";
+$request_sent = 0;
+$courses_str = ""; // This variable to be used in building insert statement
+$courses_rpi_used = array();
+
+// Class variables for true/false ip_requests
+$csc242 = 0;
+$csc315 = 0;
+$csc382 = 0;
+$csc435_635 = 0;
+$csc_other = 0;
+
+// Query Student table for new requests
+$sql = "SELECT STUDENT_ID, COURSE_RPI_USED, REQUEST_SENT FROM STUDENT";
+
+$stmt = $MYSQL_CONN->prepare($sql);
+$stmt->execute();
+$query_results = $stmt->fetchAll();
+
+// Begin building dynamic insert statement
+$sql = "INSERT INTO IP_REQUESTS (STUDENT_ID, CSC_242, CSC_315, CSC_382, CSC_435_635, OTHER)
+        VALUES (?,?,?,?,?,?)";
+
+// Begin the SQL transaction
+$MYSQL_CONN->beingTransaction();
+$stmt = $MYSQL_CONN->prepare($sql);
+
+// Loop through results, grab student id, check if request has been sent
+// if not, check if has_ip is true, if not then grab all courses taken and explode into array
+foreach($query_results as $row)
+{
+    $student_id = $row['STUDENT_ID'];
+    $request_sent = $row['REQUEST_SENT'];
+    $courses_rpi_used = explode(",", $row['COURSE_RPI_USED']);
+    
+    if($request_sent === 0)
+    {
+        // Check for proper formatting ie. only this formatting is acceptable CSC242
+        // Must loop thru $courses_rpi_used in case multiple classes
+        foreach($courses_rpi_used as $course)
+        {
+            preg_replace('/[^A-Z0-9]','',$course);
+
+            // Go thru each $course to determine which course requires, rpi
+            switch($course)
+            {
+                case 'CSC242':
+                    $csc242 = 1;
+                    break;
+                case 'CSC315':
+                    $csc315 = 1;
+                    break;
+                case 'CSC382':
+                    $csc382 = 1;
+                    break
+                case 'CSC435635':
+                    $csc435_635 = 1;
+                    break;
+                case 'OTHER':
+                    $csc_other = 1;
+                    break;
+            }
+
+        }
+        // Complete param binding and execute insert for each row
+        $stmt->bindParam(1, $student_id, PDO::PARAM_STR);
+        $stmt->bindParam(2, $csc242, PDO::PARAM_INT);
+        $stmt->bindParam(3, $csc315, PDO::PARAM_INT);
+        $stmt->bindParam(4, $csc382, PDO::PARAM_INT);
+        $stmt->bindParam(5, $csc435_635, PDO::PARAM_INT);
+        $stmt->bindParam(6, $csc_other, PDO::PARAM_INT);
+
+        // Execute and commit
+        if($stmt->execute())
+        {
+            $MYSQL_CONN->commit();
+            echo "Data insertion successful\n";
+        } else {
+            echo "Error inserting data\n";
+        }
+    }    
+}
+?>
